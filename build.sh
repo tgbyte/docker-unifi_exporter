c594ddee5c06895ae0bc78cbbf9ec2fdc07e8ec4
Thilo-Alexander Ginkel <tg@tgbyte.de>
2017-10-17 00:38:40 +0200

    v0.4.0

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker run --rm -e CGO_ENABLED=0 -v $(pwd)/bin:/go/bin golang:1.7 go get github.com/mdlayher/unifi_exporter/cmd/unifi_exporter
-docker build --no-cache -t tgbyte/unifi_exporter:v0.3.0 .
-docker push tgbyte/unifi_exporter:v0.3.0
+docker build --no-cache -t tgbyte/unifi_exporter:v0.4.0 .
+docker push tgbyte/unifi_exporter:v0.4.0
